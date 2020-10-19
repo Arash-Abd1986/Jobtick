5b849b6f4f3e4a62ee3f695c8114bc9a23468801
mehr521 <55667901+mehr521@users.noreply.github.com>
2020-10-19 15:10:35 +0330

    UI taskDetails

--- app/src/main/java/com/jobtick/activities/SinginSingupAcitivity.java
+++ app/src/main/java/com/jobtick/activities/SinginSingupAcitivity.java
@@ -2,7 +2,6 @@ package com.jobtick.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;

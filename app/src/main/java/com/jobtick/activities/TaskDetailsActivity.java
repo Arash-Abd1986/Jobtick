e91d48069aa45709400fdb1d297d125ccaa0c8ac
Mohsen Sedighi <sedighi.66@gmail.com>
2020-10-30 17:54:52 +0330

    payment overview UI is ok.

--- app/src/main/java/com/jobtick/activities/TaskDetailsActivity.java
+++ app/src/main/java/com/jobtick/activities/TaskDetailsActivity.java
@@ -1513,7 +1513,7 @@ public class TaskDetailsActivity extends ActivityBase implements OfferListAdapte
                         break;
                     case ConstantKey.BTN_MAKE_AN_OFFER:
                         int key = handleRequirementStrip();
-                        if (key == 5) {
+                        if (true) {
                             if (taskModel.getMusthave().size() == 0) {
                                 intent = new Intent(TaskDetailsActivity.this, MakeAnOfferActivity.class);
                                 bundle = new Bundle();

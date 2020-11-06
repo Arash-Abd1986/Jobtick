184def98b84664e009edda3083fbd95e821958d4
arash <aghelifar.arash@gmail.com>
2020-11-06 14:11:24 +0330

    fix issue bottom sheet styles

--- app/src/main/java/com/jobtick/fragments/PaymentEarnedBottomSheet.java
+++ app/src/main/java/com/jobtick/fragments/PaymentEarnedBottomSheet.java
@@ -33,7 +33,7 @@ public class PaymentEarnedBottomSheet extends BottomSheetDialogFragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setStyle(DialogFragment.STYLE_NORMAL, R.style.BottomSheetDialogStyle);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.bottomSheetDialogStyle);
     }
 
     @Override

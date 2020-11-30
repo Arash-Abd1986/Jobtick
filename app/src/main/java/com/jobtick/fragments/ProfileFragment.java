fa865ec0ea84d4f2988c26b14272a9597eef483d
Javad <javad@arexgo.com>
2020-11-30 22:25:27 +0330

    v0.0.7 profile_portfolio

--- app/src/main/java/com/jobtick/fragments/ProfileFragment.java
+++ app/src/main/java/com/jobtick/fragments/ProfileFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -435,7 +436,7 @@ public class ProfileFragment extends Fragment implements onProfileUpdateListener
     private void getAllProfileData() {
 
         dashboardActivity.showProgressDialog();
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, Constant.URL_PROFILE + "/" + 5,
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, Constant.URL_PROFILE + "/" + 1,
                 response -> {
                     Timber.e(response);
                     dashboardActivity.hideProgressDialog();
@@ -443,10 +444,10 @@ public class ProfileFragment extends Fragment implements onProfileUpdateListener
                         JSONObject jsonObject = new JSONObject(response);
                         Timber.e(jsonObject.toString());
                         if (jsonObject.has("data") && !jsonObject.isNull("data")) {
-
                             userAccountModel = new UserAccountModel().getJsonToModel(jsonObject.getJSONObject("data"));
                             setUpAllEditFields(userAccountModel);
                             attachmentArrayList = userAccountModel.getPortfolio();
+                            adapter.clear();
                             badgesModelArrayList = userAccountModel.getBadges();
 
 
@@ -473,6 +474,12 @@ public class ProfileFragment extends Fragment implements onProfileUpdateListener
                             }
                             badgesAdapter.addItems(badgesModelArrayList);
 
+                            if(userAccountModel.getPortfolio().size()==0)
+                            {
+                                NoPortfolio.setVisibility(View.VISIBLE);
+                            }else{
+                                NoPortfolio.setVisibility(View.GONE);
+                            }
                         } else {
                             dashboardActivity.showToast("Something went wrong", dashboardActivity);
                         }
@@ -510,18 +517,20 @@ public class ProfileFragment extends Fragment implements onProfileUpdateListener
 
     @SuppressLint("SetTextI18n")
     private void setUpAllEditFields(UserAccountModel userAccountModel) {
+        tvAboutHeading.setTypeface(txtAbout.getTypeface(), Typeface.BOLD_ITALIC);
 
         if (userAccountModel.getAbout() == null || userAccountModel.getAbout().equals("")) {
-            txtAbout.setVisibility(View.GONE);
+            txtAbout.setText("Nothing to show");
         } else {
             txtAbout.setVisibility(View.VISIBLE);
             txtAbout.setText("" + userAccountModel.getAbout());
+
         }
         if (userAccountModel.getTagline() == null ||userAccountModel.getTagline().equals("")) {
-            tvAboutHeading.setVisibility(View.GONE);
+            tvAboutHeading.setText("\"Nothing to show\"");
         } else {
             tvAboutHeading.setVisibility(View.VISIBLE);
-            tvAboutHeading.setText("" + userAccountModel.getTagline());
+            tvAboutHeading.setText("\"" + userAccountModel.getTagline()+"\"");
         }
 
         if(userAccountModel.getWorkerRatings()==null){
@@ -530,6 +539,7 @@ public class ProfileFragment extends Fragment implements onProfileUpdateListener
             txtNoReview.setVisibility(View.VISIBLE);
 
         }else{
+            noReview.setVisibility(View.GONE);
             tickerReview.setVisibility(View.VISIBLE);
             ratingbarAsTicker.setRating(userAccountModel.getWorkerRatings().getAvgRating());
             tvTickerReview.setText("("+userAccountModel.getWorkerRatings().getTotalRatings().toString()+")");
@@ -541,6 +551,7 @@ public class ProfileFragment extends Fragment implements onProfileUpdateListener
             txtNoReview.setVisibility(View.VISIBLE);
         }else{
             posterReview.setVisibility(View.VISIBLE);
+            noReview.setVisibility(View.GONE);
             ratingbarAsPoster.setRating(userAccountModel.getPosterRatings().getAvgRating());
             tvPosterReview.setText("("+userAccountModel.getPosterRatings().getTotalRatings().toString()+")");
             tvPosterCompletionRate.setText("%"+userAccountModel.getPosterRatings().getReceivedReviews().toString());
@@ -560,6 +571,74 @@ public class ProfileFragment extends Fragment implements onProfileUpdateListener
                 ivMedalBoronz.setImageResource(R.drawable.ic_max_selected);
                 break;
         }
+
+        if (userAccountModel.getIsVerifiedAccount() == 1) {
+            imgVerified.setVisibility(View.VISIBLE);
+        } else {
+            imgVerified.setVisibility(View.GONE);
+        }
+        if(userAccountModel.getPortfolio().size()==0)
+        {
+            NoPortfolio.setVisibility(View.VISIBLE);
+        }else{
+            NoPortfolio.setVisibility(View.GONE);
+        }
+        if(userAccountModel.getSkills().getExperience() == null && userAccountModel.getSkills().getExperience().size() == 0 &&
+                userAccountModel.getSkills().getLanguage() == null && userAccountModel.getSkills().getLanguage().size() == 0 &&
+                userAccountModel.getSkills().getSpecialities() == null && userAccountModel.getSkills().getSpecialities().size() == 0 &&
+                userAccountModel.getSkills().getTransportation() == null && userAccountModel.getSkills().getTransportation().size() == 0 &&
+                userAccountModel.getSkills().getEducation() == null && userAccountModel.getSkills().getEducation().size() == 0){
+            NoPortfolio.setVisibility(View.GONE);
+            lSkill.setVisibility(View.GONE);
+            lytEducation.setVisibility(View.GONE);
+            lytExperience.setVisibility(View.GONE);
+            lytLanguage.setVisibility(View.GONE);
+            lytSpecialities.setVisibility(View.GONE);
+            lytTransportation.setVisibility(View.GONE);
+            recyclerViewPortfolio.setVisibility(View.GONE);
+            lPort.setVisibility(View.GONE);
+        }
+        else{
+            if (userAccountModel.getSkills().getEducation() != null && userAccountModel.getSkills().getEducation().size() != 0) {
+                lytEducation.setVisibility(View.VISIBLE);
+                //tagEducation.setText(userAccountModel.getSkills().getEducation());
+
+                tagEducation.setTags(userAccountModel.getSkills().getEducation());
+
+            } else {
+                lytEducation.setVisibility(View.GONE);
+                tagEducation.setTags(new ArrayList<>());
+            }
+            if (userAccountModel.getSkills().getExperience() != null && userAccountModel.getSkills().getExperience().size() != 0) {
+                lytExperience.setVisibility(View.VISIBLE);
+                tagExperience.setTags(userAccountModel.getSkills().getExperience());
+            } else {
+                lytExperience.setVisibility(View.GONE);
+                tagExperience.setTags(new ArrayList<>());
+            }
+            if (userAccountModel.getSkills().getLanguage() != null && userAccountModel.getSkills().getLanguage().size() != 0) {
+                lytLanguage.setVisibility(View.VISIBLE);
+                tagLanguage.setTags(userAccountModel.getSkills().getLanguage());
+            } else {
+                lytLanguage.setVisibility(View.GONE);
+                tagLanguage.setTags(new ArrayList<>());
+            }
+            if (userAccountModel.getSkills().getSpecialities() != null && userAccountModel.getSkills().getSpecialities().size() != 0) {
+                lytSpecialities.setVisibility(View.VISIBLE);
+                tagSpecialities.setTags(userAccountModel.getSkills().getSpecialities());
+
+            } else {
+                lytSpecialities.setVisibility(View.GONE);
+                tagSpecialities.setTags(new ArrayList<>());
+            }
+            if (userAccountModel.getSkills().getTransportation() != null && userAccountModel.getSkills().getTransportation().size() != 0) {
+                lytTransportation.setVisibility(View.VISIBLE);
+                tagTransportation.setTags(userAccountModel.getSkills().getTransportation());
+            } else {
+                lytTransportation.setVisibility(View.GONE);
+                tagTransportation.setTags(new ArrayList<>());
+            }
+        }
         tagEducation.setTagTypeface(poppins_medium);
         tagSpecialities.setTagTypeface(poppins_medium);
         tagLanguage.setTagTypeface(poppins_medium);
@@ -572,88 +651,18 @@ public class ProfileFragment extends Fragment implements onProfileUpdateListener
         txtSuburb.setText(userAccountModel.getLocation());
         //txtAccountLevel.setText(""+userAccountModel.getWorkerTier().getName());
         txtLastSeen.setText("Last Seen  " + userAccountModel.getLastOnline());
+        tvViewAllReviews.setOnClickListener(v -> {
 
-        if (userAccountModel.getIsVerifiedAccount() == 1) {
-            imgVerified.setVisibility(View.VISIBLE);
-        } else {
-            imgVerified.setVisibility(View.GONE);
-            if(userAccountModel.getSkills().getExperience() == null && userAccountModel.getSkills().getExperience().size() == 0 &&
-               userAccountModel.getSkills().getLanguage() == null && userAccountModel.getSkills().getLanguage().size() == 0 &&
-               userAccountModel.getSkills().getSpecialities() == null && userAccountModel.getSkills().getSpecialities().size() == 0 &&
-               userAccountModel.getSkills().getTransportation() == null && userAccountModel.getSkills().getTransportation().size() == 0 &&
-               userAccountModel.getSkills().getEducation() == null && userAccountModel.getSkills().getEducation().size() == 0){
-                NoPortfolio.setVisibility(View.VISIBLE);
-                lSkill.setVisibility(View.GONE);
-                lytEducation.setVisibility(View.GONE);
-                lytExperience.setVisibility(View.GONE);
-                lytLanguage.setVisibility(View.GONE);
-                lytSpecialities.setVisibility(View.GONE);
-                lytTransportation.setVisibility(View.GONE);
-                recyclerViewPortfolio.setVisibility(View.GONE);
-                lPort.setVisibility(View.GONE);
-            }else{
-                if (userAccountModel.getSkills().getEducation() != null && userAccountModel.getSkills().getEducation().size() != 0) {
-                    lytEducation.setVisibility(View.VISIBLE);
-                    //tagEducation.setText(userAccountModel.getSkills().getEducation());
+            Bundle bundle = new Bundle();
+            bundle.putInt(Constant.userID, userAccountModel.getId());
+            bundle.putString("WhoIs", Constant.AS_A_WORKER);
+            //      bundle.putParcelable(Constant.userAccount, userAccountModel);
 
-                    tagEducation.setTags(userAccountModel.getSkills().getEducation());
-
-                } else {
-                    lytEducation.setVisibility(View.GONE);
-                    tagEducation.setTags(new ArrayList<>());
-                }
-                if (userAccountModel.getSkills().getExperience() != null && userAccountModel.getSkills().getExperience().size() != 0) {
-                    lytExperience.setVisibility(View.VISIBLE);
-                    tagExperience.setTags(userAccountModel.getSkills().getExperience());
-                } else {
-                    lytExperience.setVisibility(View.GONE);
-                    tagExperience.setTags(new ArrayList<>());
-                }
-                if (userAccountModel.getSkills().getLanguage() != null && userAccountModel.getSkills().getLanguage().size() != 0) {
-                    lytLanguage.setVisibility(View.VISIBLE);
-                    tagLanguage.setTags(userAccountModel.getSkills().getLanguage());
-                } else {
-                    lytLanguage.setVisibility(View.GONE);
-                    tagLanguage.setTags(new ArrayList<>());
-                }
-                if (userAccountModel.getSkills().getSpecialities() != null && userAccountModel.getSkills().getSpecialities().size() != 0) {
-                    lytSpecialities.setVisibility(View.VISIBLE);
-                    tagSpecialities.setTags(userAccountModel.getSkills().getSpecialities());
-
-                } else {
-                    lytSpecialities.setVisibility(View.GONE);
-                    tagSpecialities.setTags(new ArrayList<>());
-                }
-                if (userAccountModel.getSkills().getTransportation() != null && userAccountModel.getSkills().getTransportation().size() != 0) {
-                    lytTransportation.setVisibility(View.VISIBLE);
-                    tagTransportation.setTags(userAccountModel.getSkills().getTransportation());
-                } else {
-                    lytTransportation.setVisibility(View.GONE);
-                    tagTransportation.setTags(new ArrayList<>());
-                }
-
-            }
-
-
-
-            tvViewAllReviews.setOnClickListener(v -> {
-
-                Bundle bundle = new Bundle();
-                bundle.putInt(Constant.userID, userAccountModel.getId());
-                bundle.putString("WhoIs", Constant.AS_A_WORKER);
-                //      bundle.putParcelable(Constant.userAccount, userAccountModel);
-
-                startActivity(new Intent(dashboardActivity, ReviewsActivity.class)
-                        .putExtras(bundle));
-            });
-
-            onChangeTabBiography();
-            onChangeTabUser();
-       /*
+            startActivity(new Intent(dashboardActivity, ReviewsActivity.class)
+                    .putExtras(bundle));
+        });
         onChangeTabBiography();
         onChangeTabUser();
-       */
-        }
     }
 
     public void onViewClicked() {

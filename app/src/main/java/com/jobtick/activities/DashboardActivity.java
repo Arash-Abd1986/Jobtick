899bc8d418a9fee84651fb354dafaee55b1a8b7c
Javad <javad@arexgo.com>
2020-12-02 20:31:01 +0330

    v0.1.0 last_commit

--- app/src/main/java/com/jobtick/activities/DashboardActivity.java
+++ app/src/main/java/com/jobtick/activities/DashboardActivity.java
@@ -90,13 +90,16 @@ public class DashboardActivity extends ActivityBase implements NavigationView.On
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         toolbar = findViewById(R.id.toolbar);
         // Tools.clearSystemBarLight(this);
         toolbar.setElevation(0);
         sessionManager = new SessionManager(this);
-        Log.d("Access_token",sessionManager.getAccessToken());
         onProfileupdatelistenerSideMenu = this;
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);

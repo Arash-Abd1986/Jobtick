cf8562e7f8ec107bc854ebccf22c210b1af7faa5
upornot <javad@arexgo.com>
2021-01-05 09:08:40 +0330

    v0.6.8 code_cleaning

--- app/src/main/java/com/jobtick/activities/TaskDetailsActivity.java
+++ app/src/main/java/com/jobtick/activities/TaskDetailsActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

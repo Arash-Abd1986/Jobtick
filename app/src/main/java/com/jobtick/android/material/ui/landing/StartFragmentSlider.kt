d38b30a251a6d7ce61cb4410d67d0f97b402c513
Jalil <taghinezhad.jalil@yahoo.com>
2022-07-27 11:40:41 +0430

    slider bottom anim

--- app/src/main/java/com/jobtick/android/material/ui/landing/StartFragmentSlider.kt
+++ app/src/main/java/com/jobtick/android/material/ui/landing/StartFragmentSlider.kt
@@ -2,23 +2,30 @@ package com.jobtick.android.material.ui.landing
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.button.MaterialButton
 import com.jobtick.android.R
 import com.jobtick.android.activities.DashboardActivity
 import com.jobtick.android.utils.Constant
 import com.jobtick.android.utils.SessionManager
+import com.jobtick.android.utils.dpToPx
+import com.jobtick.android.utils.pxToDp
 
 class StartFragmentSlider : Fragment(), ViewPagerAdapter.ItemClick {
     private lateinit var pager: ViewPager2
     private lateinit var signIn: MaterialButton
+    private lateinit var radioBtnOne: RadioButton
+    private lateinit var radioBtnTwo: RadioButton
+    private lateinit var radioBtnThree: RadioButton
+    private lateinit var radioGroup: RadioGroup
     private lateinit var imBack: AppCompatImageView
     private lateinit var activity: OnboardingActivity
     private lateinit var sessionManagerA: SessionManager
@@ -43,13 +50,37 @@ class StartFragmentSlider : Fragment(), ViewPagerAdapter.ItemClick {
         signIn = view.findViewById(R.id.signIn)
         pager = view.findViewById(R.id.pager)
         imBack = view.findViewById(R.id.imBack)
+        radioGroup = view.findViewById(R.id.radioGroup)
+        radioBtnOne = view.findViewById(R.id.radioBtnOne)
+        radioBtnTwo = view.findViewById(R.id.radioBtnTwo)
+        radioBtnThree = view.findViewById(R.id.radioBtnThree)
+        radioBtnOne.layoutParams = radioBtnOne.layoutParams.also { layoutParams ->
+            layoutParams.width = (24).dpToPx()
+        }
+        radioBtnTwo.layoutParams = radioBtnTwo.layoutParams.also { layoutParams ->
+            layoutParams.width = (8).dpToPx()
+        }
+        radioBtnThree.layoutParams = radioBtnThree.layoutParams.also { layoutParams ->
+            layoutParams.width = (8).dpToPx()
+        }
         val adapter = ViewPagerAdapter(data)
         pager.adapter = adapter
         pager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
         pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
+
+                radioGroup.check(radioGroup.getChildAt(position).id);
                 when (position) {
                     0 -> {
+                        radioBtnOne.layoutParams = radioBtnOne.layoutParams.also { layoutParams ->
+                            layoutParams.width = (24).dpToPx()
+                        }
+                        radioBtnTwo.layoutParams = radioBtnTwo.layoutParams.also { layoutParams ->
+                            layoutParams.width = (8).dpToPx()
+                        }
+                        radioBtnThree.layoutParams = radioBtnThree.layoutParams.also { layoutParams ->
+                            layoutParams.width = (8).dpToPx()
+                        }
                         imBack.setImageDrawable(
                             ContextCompat.getDrawable(
                                 requireContext(),
@@ -59,6 +90,15 @@ class StartFragmentSlider : Fragment(), ViewPagerAdapter.ItemClick {
                         next.text = "Next"
                     }
                     1 -> {
+                        radioBtnOne.layoutParams = radioBtnOne.layoutParams.also { layoutParams ->
+                            layoutParams.width = (8).dpToPx()
+                        }
+                        radioBtnTwo.layoutParams = radioBtnTwo.layoutParams.also { layoutParams ->
+                            layoutParams.width = (24).dpToPx()
+                        }
+                        radioBtnThree.layoutParams = radioBtnThree.layoutParams.also { layoutParams ->
+                            layoutParams.width = (8).dpToPx()
+                        }
                         imBack.setImageDrawable(
                             ContextCompat.getDrawable(
                                 requireContext(),
@@ -68,6 +108,15 @@ class StartFragmentSlider : Fragment(), ViewPagerAdapter.ItemClick {
                         next.text = "Next"
                     }
                     2 -> {
+                        radioBtnOne.layoutParams = radioBtnOne.layoutParams.also { layoutParams ->
+                            layoutParams.width = (8).dpToPx()
+                        }
+                        radioBtnTwo.layoutParams = radioBtnTwo.layoutParams.also { layoutParams ->
+                            layoutParams.width = (8).dpToPx()
+                        }
+                        radioBtnThree.layoutParams = radioBtnThree.layoutParams.also { layoutParams ->
+                            layoutParams.width = (24).dpToPx()
+                        }
                         imBack.setImageDrawable(
                             ContextCompat.getDrawable(
                                 requireContext(),
